432100647217c0944b388cc8b1aaa6c581cf62d5
Julio Cesar <julio-cesar@ufrj.br>
2012-02-28 02:23:33 -0300

    Alterações na classe app

--- system/app.php
+++ system/app.php
@@ -6,7 +6,10 @@
 			$control_template->view($file_template,$data);
 		}
 			
-		
+		protected function callapp($app_name){
+			$app = new $app_name();
+			$app->call();
+		}
 		
 		abstract public function call();
 		
