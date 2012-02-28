106b74340c1d8b9d767b1eb2902d7739db5ab9bf
Julio Cesar <julio-cesar@ufrj.br>
2012-02-28 01:46:36 -0300

    Alteração em relação a primeira versão

--- app/example.php
+++ app/example.php
@@ -1,9 +1,7 @@
 <?php
 	class example extends app{
 		public function call(){
-			$control_template = new default_template();
-			$control_template->view("example",array("title"=>"EXAMPLE","content"=>"This is a example app!"));
-			$calendar = new calendar(2,2012);
+			$this->loadview(new default_template(),"example",array("title"=>"EXAMPLE","content"=>"This is a example app!"));
 		}
 	}
 
--- app/router.php
+++ app/router.php
@@ -1,5 +1,5 @@
 <?php
 
-	$default_app = "backman";
+	$default_app = "example";
 
 ?>
