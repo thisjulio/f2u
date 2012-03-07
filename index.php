5c726196dc00d9dc7b0d53723093bb29e5a5ec96
Julio Cesar <julio-cesar@ufrj.br>
2012-03-07 13:56:40 -0300

    Reorganização da index

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 	require "system/conf.php";
 	require "app/router.php";
 	require "system/config.php";
-	$et = new exectime();
 	function __autoload($classname){
 		foreach($GLOBALS['path'] as $p)
-			if(file_exists("$p{$classname}.php"))
+			if(file_exists("$p{$classname}.php")){
 				require_once "$p{$classname}.php";
+			}
 	}
-	
 	$app = new $default_app();
 	$app->run();
-	$et->end_exectime();
 ?>
